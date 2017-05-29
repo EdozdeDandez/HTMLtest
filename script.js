0ea559cfe08894c89a1c05f95eb32300ad6d8eb6
EdozdeDandez <edith.edidan.dande@gmail.com>
2017-05-29 05:56:43 +0000

    With css file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function draw(){
 			var c = document.getElementById("can");
 			var ctx = c.getContext("2d");
-			ctx.font = "30px Arial";
+			ctx.font = "50px Arial";
 			ctx.strokeText("Hallo world!",10,50);
 		}
